da0512bdb4ca7878f9a7f463f20e2f23d92a876d
jrenoj <jfchase042@gmail.com>
2024-07-26 14:16:13 -0700

    increased slide time per image

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ function showSlides() {
 	slideIndex++;
 	if (slideIndex > slides.length) {slideIndex = 1}    
 	slides[slideIndex-1].style.display = "block";  
-	setTimeout(showSlides, 6500); 
+	setTimeout(showSlides, 10000); 
 }
 
 document.addEventListener("DOMContentLoaded", function() {
